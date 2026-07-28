4cf4a6e5c0fba5dfe3529957ad37ee0c117597e5
phongthien <phongthien1995@gmail.com>
2026-07-28 13:54:57 +0700

    Update main.c

--- main/main.c
+++ main/main.c
@@ -1,44 +1,19 @@
-#include <stdio.h>
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
 #include "driver/gpio.h"
 
-#include "lcd.h"
-
-#define LED_PIN GPIO_NUM_4
+#define LED_GPIO GPIO_NUM_4
 
 void app_main(void)
 {
-    gpio_config_t io = {
-        .pin_bit_mask = (1ULL << LED_PIN),
-        .mode = GPIO_MODE_OUTPUT,
-        .pull_up_en = GPIO_PULLUP_DISABLE,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_DISABLE
-    };
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
 
-    gpio_config(&io);
-
-    // Khởi tạo LCD
-    lcd_init();
-
-    // Xóa màn hình
-    lcd_clear();
-
-    // Hiển thị chữ Hello
-    lcd_draw_string(20, 3, "Hello");
-
-    // Cập nhật LCD
-    lcd_update();
-
-    while (1)
-    {
-        gpio_set_level(LED_PIN, 1);
+    while (1) {
+        gpio_set_level(LED_GPIO, 1);
         vTaskDelay(pdMS_TO_TICKS(500));
 
-        gpio_set_level(LED_PIN, 0);
+        gpio_set_level(LED_GPIO, 0);
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
